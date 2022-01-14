5f6507a5405b51a5512ee8e22bc554d375b36e43
hammerhx <amerhouchaimi1980@gmail.com>
2022-01-14 13:29:09 -0300

    Mais estudos de Javascript

---
+++ expressoes.js
@@ -0,0 +1,19 @@
+// declaração de função
+
+function minhaFuncao(param) {
+    //bloco de código
+}
+
+minhaFuncao ("param")
+
+// expressão de função
+
+const soma = function(num1, num2) {return num1 + num2}
+console.log(soma(1, 1))
+
+// diferença principal???
+// Funções e var são "listadas" no topodo arquivo
+
+function apresentar () {
+    return "olá";
+}
---
+++ parametros.js
@@ -0,0 +1,21 @@
+// parâmetros de função
+
+              //2       //2
+function soma(numero1, numero2) {
+    return numero1 + numero2;
+}
+
+console.log(soma(2, 2))
+console.log(soma(245, 20))
+console.log(soma(-500, 60))
+
+// parâmetros x argumentos
+
+// ordem dos parâmetros altera o produto - Sempre lembrar
+
+function multiplica(numero1, numero2){
+    return numero1* numero2;
+}
+
+                            //9         //6
+console.log(multiplica(soma(4, 5), soma(3, 3)))
